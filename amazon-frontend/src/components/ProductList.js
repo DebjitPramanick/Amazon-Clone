2197988e67d3b6efe962ea33a4a4eb4a6a8388e4
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-26 12:55:34 +0530

    Verson 1

--- amazon-frontend/src/components/ProductList.js
+++ amazon-frontend/src/components/ProductList.js
@@ -1,11 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import '../styles/ProductList.css'
-import {data} from "../data"
 import Product from './Product'
+import axios from "../Axios"
 
 const ProductList = () => {
 
-    const [products,setProducts] = useState(data);
+    const [products,setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchdata = async()=>{
+            const {data} = await axios.get("/api/products");
+            setProducts(data);
+        };
+        fetchdata();
+    }, [])
+
+    console.log(products);
 
 
     return (
