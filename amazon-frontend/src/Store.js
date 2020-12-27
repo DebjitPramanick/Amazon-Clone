bfbc37eddb8efe6444577046fef607194f988db1
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-27 15:17:54 +0530

    Version 1

--- amazon-frontend/src/Store.js
+++ amazon-frontend/src/Store.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import {data} from "./data"
 import { prodcutListReducer } from './reducers/ProductReducer';
 
 const initialState = {};
--- amazon-frontend/src/actions/ProdcutActions.js
+++ amazon-frontend/src/actions/ProdcutActions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "../Axios";
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_SUCCESS } from "../constants/ProductConstants";
 
 export const listProducts = () => async (dispatch) =>{
@@ -8,6 +8,7 @@ export const listProducts = () => async (dispatch) =>{
 
     try{
         const {data} = await axios.get("/api/products");
+   
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data
