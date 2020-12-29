6d52b025d352cf746f6cfaa0e5ee6ef0fb0e5d53
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-29 10:36:44 +0530

    Version 1

--- amazon-frontend/src/pages/Cart.js
+++ amazon-frontend/src/pages/Cart.js
@@ -1,6 +1,10 @@
 import React,{useEffect} from 'react'
 import { useDispatch,useSelector } from 'react-redux';
 import { addToCart } from '../actions/CartAction';
+import { Link } from 'react-router-dom';
+import MessageBox from "../components/MessageBox";
+import "../styles/Cart.css"
+
 
 const Cart = (props) => {
 
@@ -11,6 +15,7 @@ const Cart = (props) => {
 
     
     const cart = useSelector((state) => state.cart);
+    const { cartItems, error } = cart;
 
     const dispatch = useDispatch();
 
@@ -20,9 +25,57 @@ const Cart = (props) => {
         }
     }, [dispatch, productID, qty])
 
+
+    const removeFromCart = (id) =>{
+        //fewwf
+    }
+
     return (
-        <div>
-            
+        <div className="row-top">
+            <div className="col-4">
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 ? (
+                    <MessageBox>
+                        Cart is empty. <Link to="/">Go Shopping</Link>
+                    </MessageBox>
+                ) : (
+                    <ul>
+                        {
+                            cartItems.map((item)=>(
+                                <li key={item.product}>
+                                    <div className="row1">
+                                        <div className="small">
+                                            <img src={item.image}
+                                            alt= ""
+                                            ></img>
+                                        </div>
+
+                                        <div className="min-30">
+                                            <Link to={`/prodcut/${item.product}`}>{item.name}</Link>
+                                        </div>
+                                        <div>
+                                            <select value={item.qty} 
+                                            onChange={e => 
+                                            dispatch(addToCart(item.product),
+                                            Number(e.target.value))}>
+                                            {
+                                                [...Array(item.stock).keys()].map((x)=>(
+                                                    <option value={x+1}>{x+1}</option>
+                                                ))
+                                            }
+                                            </select>
+                                        </div>
+                                        <p>${item.price}</p>
+                                        <div>
+                                            <button type="button" onClick={() => removeFromCart(item.product)}></button>
+                                        </div>
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                )}
+            </div>
         </div>
     )
 }
