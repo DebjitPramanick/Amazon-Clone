5903665b5a55651d85050fbd5430e0c65d3a8c07
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-30 14:25:42 +0530

    Version 1

--- amazon-backend/utils.js
+++ amazon-backend/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (user) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-    }, process.env.JWT_SECRET,
+    }, process.env.JWT_SECRET || 'somethingsecret',
     {
         expiresIn: '30d',
     });
