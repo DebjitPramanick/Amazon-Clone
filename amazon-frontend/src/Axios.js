b78b80bab18574bc7bcb58c598f02cb86768107b
DebjitPramanick <dpramanick9990@gmail.com>
2021-01-09 02:10:22 +0530

    Version 1

--- amazon-frontend/src/Axios.js
+++ amazon-frontend/src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://amazon-clone-backend-2021.herokuapp.com/"
 })
 
 export default instance
